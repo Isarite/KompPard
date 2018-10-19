c897d39805f152754e1df5352aef13403c9f289e
Gediminas <8672277+GedasFX@users.noreply.github.com>
2018-10-19 12:05:05 +0300

    Initial project commit

---
+++ Project/Project/Controllers/InventoryController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Project.Controllers
+{
+    public class InventoryController : Controller
+    {
+        public IActionResult Index()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ Project/Project/Controllers/ReportsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Project.Controllers
+{
+    public class ReportsController : Controller
+    {
+        public IActionResult Index()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
