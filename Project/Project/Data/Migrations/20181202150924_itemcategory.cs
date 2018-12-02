4aa91dacd9a5e2efdd7f7bdf40e6575d5f9f86b3
Gediminas <8672277+GedasFX@users.noreply.github.com>
2018-12-02 19:27:59 +0200

    Added cart mechanics

---
+++ Project/Project/Data/Migrations/20181202150924_itemcategory.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Project.Data.Migrations
+{
+    public partial class itemcategory : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "InventoryItems",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "CategoryId",
+                table: "InventoryItems",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_InventoryItems_CategoryId",
+                table: "InventoryItems",
+                column: "CategoryId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_InventoryItems_InventoryItemCategories_CategoryId",
+                table: "InventoryItems",
+                column: "CategoryId",
+                principalTable: "InventoryItemCategories",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_InventoryItems_InventoryItemCategories_CategoryId",
+                table: "InventoryItems");
+
+            migrationBuilder.DropIndex(
+                name: "IX_InventoryItems_CategoryId",
+                table: "InventoryItems");
+
+            migrationBuilder.DropColumn(
+                name: "CategoryId",
+                table: "InventoryItems");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "InventoryItems",
+                nullable: true,
+                oldClrType: typeof(string));
+        }
+    }
+}
