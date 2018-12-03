a407ca413a8175a030d3ad5e44ab7b116b5cccc8
Isarite <rujsamod@gmail.com>
2018-12-03 13:52:21 +0200

    Added range constraints for feedback rating value

--- Project/Project/Models/Feedback.cs
+++ Project/Project/Models/Feedback.cs
@@ -12,6 +12,7 @@ namespace Project.Models
         public int ItemId { get; set; }
 
         public string Comment { get; set; }
+        [Range(0, 5, ErrorMessage = "Value for {0} must be between {1} and {2}.")]
         public int Rating { get; set; }
 
         public virtual ApplicationUser User { get; set; }
